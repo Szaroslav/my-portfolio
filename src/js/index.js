06129843fc2f7363ed00fd6ef3557fc70f58f2bc
Jakub <jakub.szaredko@gmail.com>
2021-05-24 00:34:25 +0200

    Install TypewriterJS, add typewriter effect to one of headings

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import Parallax from 'parallax-js';
+import Typewriter from 'typewriter-effect/dist/core';
 
 window.addEventListener('DOMContentLoaded', () => {
     const parallax = new Parallax(document.querySelector('.portfolio__bg'), {
@@ -7,5 +8,12 @@ window.addEventListener('DOMContentLoaded', () => {
         selector: '.bg__element'
     });
 
-    console.log("lol");
+    const typewriter = new Typewriter('.portfolio__heading-2', {
+        strings: [ 'Jakub', 'Computer programmer', 'Fullstack developer' ],
+        autoStart: true,
+        loop: true,
+        pauseFor: 5000,
+        cursor: '_',
+        skipAddStyles: true
+    });
 });
