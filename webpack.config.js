76560b398204743d0bf838e6b4866d141c195dd4
Jakub <jakub.szaredko@gmail.com>
2021-05-26 11:46:20 +0200

    Install html-loader, configure HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,10 @@ module.exports = {
         path: path.resolve(__dirname, 'dist')
     },
     plugins: [
-        new MiniCssExtractPlugin()
+        new MiniCssExtractPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/html/template.html'
+        })
     ],
     module: {
         rules: [
@@ -30,12 +33,20 @@ module.exports = {
                 }
             },
             {
+                test: /\.html/i,
+                loader: 'html-loader'
+            },
+            {
                 test: /\.s[ac]ss$/i,
                 use: [
                     MiniCssExtractPlugin.loader,
                     'css-loader',
                     'sass-loader'
                 ]
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                type: 'asset/resource'
             }
         ]
     }
