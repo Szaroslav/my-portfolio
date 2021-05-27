00198a2df2b5c116ba182c455313380b95fd9a8a
Jakub <jakub.szaredko@gmail.com>
2021-05-27 19:16:23 +0200

    Install html-webpack-inline-svg-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,30 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackInlineSVGPlugin = require('html-webpack-inline-svg-plugin');
 
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
     devtool: 'inline-source-map',
     devServer: {
-        contentBase: './dist',
+        contentBase: path.resolve(__dirname, 'dist'),
         hot: true
     },
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
     },
+    performance: {
+        maxEntrypointSize: 400000
+    },
     plugins: [
         new MiniCssExtractPlugin(),
         new HtmlWebpackPlugin({
             template: './src/html/template.html'
+        }),
+        new HtmlWebpackInlineSVGPlugin({
+            runPreEmit: false
         })
     ],
     module: {
